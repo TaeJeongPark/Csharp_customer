d2da1872790bf0dbb93c3daa4c30d1577be541ab
TaeJeongPark <ksmpjc99@gmail.com>
2023-09-08 14:53:53 +0900

    초기 커밋.

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_select = new System.Windows.Forms.Button();
+            this.btn_insert = new System.Windows.Forms.Button();
+            this.list_result = new System.Windows.Forms.ListView();
+            this.tb_birth = new System.Windows.Forms.TextBox();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(282, 123);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(200, 39);
+            this.btn_select.TabIndex = 21;
+            this.btn_select.Text = "조회";
+            this.btn_select.UseVisualStyleBackColor = true;
+            // 
+            // btn_insert
+            // 
+            this.btn_insert.Location = new System.Drawing.Point(57, 123);
+            this.btn_insert.Name = "btn_insert";
+            this.btn_insert.Size = new System.Drawing.Size(200, 39);
+            this.btn_insert.TabIndex = 20;
+            this.btn_insert.Text = "입력";
+            this.btn_insert.UseVisualStyleBackColor = true;
+            // 
+            // list_result
+            // 
+            this.list_result.HideSelection = false;
+            this.list_result.Location = new System.Drawing.Point(57, 192);
+            this.list_result.Name = "list_result";
+            this.list_result.Size = new System.Drawing.Size(425, 204);
+            this.list_result.TabIndex = 19;
+            this.list_result.UseCompatibleStateImageBehavior = false;
+            // 
+            // tb_birth
+            // 
+            this.tb_birth.Location = new System.Drawing.Point(396, 58);
+            this.tb_birth.Name = "tb_birth";
+            this.tb_birth.Size = new System.Drawing.Size(86, 28);
+            this.tb_birth.TabIndex = 18;
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(283, 58);
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(86, 28);
+            this.tb_email.TabIndex = 17;
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(171, 58);
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(86, 28);
+            this.tb_name.TabIndex = 16;
+            // 
+            // tb_id
+            // 
+            this.tb_id.Location = new System.Drawing.Point(57, 58);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(86, 28);
+            this.tb_id.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(393, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 18);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "출생연도";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(280, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "이메일";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "이름";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "아이디";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 450);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.btn_insert);
+            this.Controls.Add(this.list_result);
+            this.Controls.Add(this.tb_birth);
+            this.Controls.Add(this.tb_email);
+            this.Controls.Add(this.tb_name);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "GUI 데이터베이스 연결";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_select;
+        private System.Windows.Forms.Button btn_insert;
+        private System.Windows.Forms.ListView list_result;
+        private System.Windows.Forms.TextBox tb_birth;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
